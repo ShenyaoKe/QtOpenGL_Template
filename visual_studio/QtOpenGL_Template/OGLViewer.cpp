d08a0695d386799b5fef672da8967f29b13e0cba
ShenyaoKe <akika@tamu.edu>
2015-09-08 10:08:20 -0500

    test

--- visual_studio/QtOpenGL_Template/OGLViewer.cpp
+++ visual_studio/QtOpenGL_Template/OGLViewer.cpp
@@ -20,9 +20,9 @@ void OGLViewer::initializeGL()
 		+1.0f, -1.0f
 	};
 
-	GLuint myBufferID;
-	glGenBuffers(1, &myBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
+	GLuint bufferidx;
+	glGenBuffers(1, &bufferidx);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferidx);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
