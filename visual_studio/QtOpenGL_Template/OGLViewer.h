345711a188ab4a5b6396b79b1ef15ede4b054e27
akika <akika@tamu.edu>
2016-02-03 12:10:22 -0600

    modified template to better workflow.

--- visual_studio/QtOpenGL_Template/OGLViewer.h
+++ visual_studio/QtOpenGL_Template/OGLViewer.h
@@ -15,27 +15,12 @@
 //#include <QGLFunctions>
 
 #include "OpenGL_Utils/GLSLProgram.h"
+
 #include "Math/MathUtil.h"
 #include "Image/ImageData.h"
 #include "Geometry/Mesh.h"
 //#include "Math/Matrix4D.h"
-#include "Camera/Camera.h"
-#include "Accel/KdTreeAccel.h"
-
-static Mesh *model_mesh;
-static GLfloat* model_verts;// vertices vbo
-static GLfloat* model_uvs;// Texture coordinates vbo
-static GLfloat* model_norms;// Normal coordinates vbo
-static GLint* model_uids;
-static int model_vbo_size;// Triangle face numbers
-
-static Mesh *box_mesh;// Display object
-static GLfloat* box_verts;// vertices vbo
-static GLfloat* box_uvs;// Texture coordinates vbo
-static GLfloat* box_norms;// Normal coordinates vbo
-static GLint* box_idxs;
-static int box_vbo_size;// Triangle face numbers
-static GLSLProgram* shader;// OpenGL shader program
+#include "Camera/perspCamera.h"
 
 static int model_mat_loc;// Uniform matrix location
 static GLfloat model_mat[16];// Uniform matrix buffer
@@ -43,16 +28,6 @@ static int view_mat_loc;// Uniform matrix location
 static GLfloat view_mat[16];
 static int proj_mat_loc;// Porjection matrix location
 static GLfloat proj_mat[16];
-static int sel_id_loc;
-//////////////////////////////////////////////////////////////////////////
-// Acceleration
-//////////////////////////////////////////////////////////////////////////
-static vector<Shape*> triangleList;
-static KdTreeAccel *mytree;
-static ImageData* img;
-//////////////////////////////////////////////////////////////////////////
-// Sphere Attribute
-//////////////////////////////////////////////////////////////////////////
 
 const double eps = 5e-4;
 
@@ -102,7 +77,20 @@ private:
 	int m_selectMode;
 private: // OpenGL variables
 	int display_mode = 0;
-	vector<GLuint> vao_handles;
+
+	Mesh *box_mesh;// Display object
+	vector<GLfloat> box_verts;// vertices vbo
+	vector<GLfloat> box_uvs;// Texture coordinates vbo
+	vector<GLfloat> box_norms;// Normal coordinates vbo
+	GLuint box_vert_vbo, box_norm_vbo, box_vao;
+
+	Mesh *model_mesh;
+	vector<GLfloat> model_verts;// vertices vbo
+	vector<GLfloat> model_uvs;// Texture coordinates vbo
+	vector<GLfloat> model_norms;// Normal coordinates vbo
+	GLuint model_vert_vbo, model_norm_vbo, model_uv_vbo, model_vao;
+
+	GLSLProgram* shader;// OpenGL shader program
 
 	friend class MainWindow;
 };
